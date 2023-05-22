9d4b933f47a1c914e6fbd61f7bebc0189dbb33bf
Ulises Ale <ulisesaale@gmail.com>
2023-05-22 14:37:16 -0300

    DB Python

---
+++ src/conection_mysql.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+midb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Paemig.021802",
+    database="proyectointegradorv01"
+)
+print(midb)
