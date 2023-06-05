634d42f68fbe863d18c66cbc26e71e95f8517271
Ulises Ale <ulisesaale@gmail.com>
2023-06-05 20:45:01 -0300

    midb

--- src/conection_mysql.py
+++ src/conection_mysql.py
@@ -1,5 +1,23 @@
 import mysql.connector
 
+class   Conexion:
+    def __init__(self, host, user, password, database):
+        self.host= host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+        
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host = self.host,
+            user = self.user,
+            password = self.password,
+            database = self.database
+        )
+        if self.connection.is_connected():
+            print("Conexion existosa")
+
 midb = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -7,3 +25,10 @@ midb = mysql.connector.connect(
     database="proyectointegradorv01"
 )
 print(midb)
+
+cursor = midb.cursor()
+
+cursor.execute("SHOW TABLES")
+
+for x in cursor:
+    print(x)
---
+++ src/principal.py
@@ -0,0 +1,5 @@
+import conexion
+
+midb = conexion.Conexion(host="localhost", user="root", password="Paemig.021802", database= "proyectointegradorv01")
+
+midb.connect()
