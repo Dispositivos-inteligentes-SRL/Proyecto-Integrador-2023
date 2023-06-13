b193af2984c6d7a2e3fca98eccafd3c120522ef9
Ulises Ale <ulisesaale@gmail.com>
2023-06-12 23:05:05 -0300

    V01

--- ConexionMySQL/conexion.py
+++ ConexionMySQL/conexion.py
@@ -1,13 +1,13 @@
 import mysql.connector
 
-class   Conexion:
+class Conexion:
     def __init__(self, host, user, password, database):
-        self.host = host,
-        self.user = user,
-        self.password = password,
-        self.database = database,
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
         self.connection = None
-        
+
     def connect(self):
         self.connection = mysql.connector.connect(
             host=self.host,
@@ -16,4 +16,32 @@ class   Conexion:
             database=self.database
         )
         if self.connection.is_connected():
-            print("Conexion existosa")
+            print("Conexión exitosa")
+
+    def disconnect(self):
+        if self.connection.is_connected():
+            self.connection.close()
+            print("Desconexión")
+
+    def leer_propietarios(self):
+        if self.connection.is_connected():
+            cursor = self.connection.cursor()
+            query = "SELECT * FROM Propietario"
+            cursor.execute(query)
+            propietarios = cursor.fetchall()
+            cursor.close()
+            return propietarios
+        else:
+            print("No hay conexión a la base de datos")
+
+    def ingresar_propietario(self, id_propietario, nombre, apellido, fecha_nacimiento):
+        if self.connection.is_connected():
+            cursor = self.connection.cursor()
+            query = "INSERT INTO propietario (id_propietario, Nombre, Apellido, FechaNacimiento) VALUES (%s, %s, %s, %s)"
+            values = (id_propietario, nombre, apellido, fecha_nacimiento)
+            cursor.execute(query, values)
+            self.connection.commit()
+            cursor.close()
+            print("Datos insertados correctamente")
+        else:
+            print("No hay conexión a la base de datos")
--- ConexionMySQL/principal.py
+++ ConexionMySQL/principal.py
@@ -1,5 +1,26 @@
 import conexion
 
-midb = conexion.Conexion(host="localhost", user="root", password="Paemig.021802", database="proyectointegradorv01")
+# Crear una instancia del conector de base de datos
+conexion_bd = conexion.Conexion(host="localhost", user="root", password="Paemig.021802", database="proyectointegradorv01")
 
-midb.connect()
+# Conectar a la base de datos
+conexion_bd.connect()
+
+# Leer los propietarios desde la tabla
+propietarios = conexion_bd.leer_propietarios()
+for propietario in propietarios:
+    print(propietario)
+
+print("Fin tabla Propietarios")
+
+conexion_bd.ingresar_propietario(id_propietario=1, nombre="Ulises", apellido="Ale", fecha_nacimiento="1979-10-19")
+
+# Leer los propietarios desde la tabla
+propietarios = conexion_bd.leer_propietarios()
+for propietario in propietarios:
+    print(propietario)
+
+print("Fin tabla Propietarios")
+
+# Desconectar de la base de datos
+conexion_bd.disconnect()
