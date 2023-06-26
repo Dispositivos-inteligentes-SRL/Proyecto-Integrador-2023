e07f0c10579cf9528955f09290d08b6431aa621c
Ulises Ale <ulisesaale@gmail.com>
2023-06-25 21:20:11 -0300

    Borrado

--- Base de datos/Tablas.sql
+++
@@ -1,3 +0,0 @@
-
-
-CREATE TABLE Propietario (id_Propietario INT, Nombre varchar(50), Apellido varchar(50), FechaNacimiento datetime)
--- Base de datos/proyectoIntegradorv01.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM proyectointegradorv01.propietario;
