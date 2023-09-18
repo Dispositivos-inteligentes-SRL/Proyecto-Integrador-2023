c8042362ddef99eec470b8b0dbe4cf405fe7e882
Ulises Ale <129899978+ulisesaale@users.noreply.github.com>
2023-09-18 09:08:02 -0300

    Update basededatos.py

--- Proyecto Integrador 2 - Programacion/Entregas/Entrega 2/basededatos.py
+++ Proyecto Integrador 2 - Programacion/Entregas/Entrega 2/basededatos.py
@@ -16,13 +16,13 @@ datos_manuales = [           # Simula la lectura de datos
     (24.8, 46.5),
 ]
 
-for temperatura, humedad in datos_manuales:
+for Temperatura, Humedad in datos_manuales:
     # Inserta los datos en la base de datos
-    insert_query = "INSERT INTO lectura_sensor (temperatura, humedad) VALUES (%s, %s)"
-    cursor.execute(insert_query, (temperatura, humedad))
+    insert_query = "INSERT INTO lectura_sensor (Temperatura, Humedad) VALUES (%s, %s)"
+    cursor.execute(insert_query, (Temperatura, Humedad))
     conn.commit()
 
-    print(f"Datos insertados: Temperatura={temperatura}, Humedad={humedad}")
+    print(f"Datos insertados: Temperatura={Temperatura}, Humedad={Humedad}")
 
 cursor.close()
 conn.close()
